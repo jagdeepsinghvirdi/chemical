597f46218edb5bbae30a93239319b1519a6b7eb5
finbyz <info@finbyz.com>
2025-03-04 16:44:55 +0530

    fix: valuation rate change based on batch no

--- chemical/public/js/doctype_js/delivery_note.js
+++ chemical/public/js/doctype_js/delivery_note.js
@@ -332,14 +332,32 @@ frappe.ui.form.on("Delivery Note Item", {
 
     batch_no: function (frm, cdt, cdn) {
         let d = locals[cdt][cdn];
-        frappe.db.get_value("Batch", d.batch_no, ['packaging_material', 'packing_size', 'lot_no', 'batch_yield', 'concentration', 'valuation_rate'], function (r) {
+        frappe.db.get_value("Batch", d.batch_no, ['packaging_material', 'packing_size', 'lot_no', 'batch_yield', 'concentration'], function (r) {
             console.log(r.packing_size)
             frappe.model.set_value(cdt, cdn, 'packaging_material', r.packaging_material);
             frappe.model.set_value(cdt, cdn, 'packing_size', r.packing_size);
             frappe.model.set_value(cdt, cdn, 'lot_no', r.lot_no);
             frappe.model.set_value(cdt, cdn, 'batch_yield', r.batch_yield);
             frappe.model.set_value(cdt, cdn, 'concentration', r.concentration);
-            frappe.model.set_value(cdt, cdn, 'rate', r.valuation_rate);
         });
+
+        frappe.xcall('erpnext.stock.utils.get_incoming_rate', {
+            args: {
+                "item_code": d.item_code,
+                "warehouse": frm.doc.set_warehouse,
+                "posting_date": frm.doc.posting_date,
+                "posting_time": frm.doc.posting_time,
+                "qty": d.qty,
+                "serial_no": d.serial_no,
+                "batch_no": d.batch_no,
+                "voucher_type": "Delivery Note",
+                "company": frm.doc.company,
+                "allow_zero_valuation_rate": d.allow_zero_valuation_rate
+            }
+        }).then(rate=>{
+            frappe.model.set_value(cdt, cdn, 'rate', rate);
+            frappe.model.set_value(cdt, cdn, 'incoming_rate', rate);
+        })
+
     }
 });
